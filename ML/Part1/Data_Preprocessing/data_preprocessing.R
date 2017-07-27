6c9f701938ab841e6587cf2a7dd41aaa1cea860a
Johnson Lai <jlwhoo7@gmail.com>
2017-07-28 01:16:30 +0800

    [Chapter 1] Done splitting dataset into Training set and Test set

--- ML/Part1/Data_Preprocessing/data_preprocessing.R
+++ ML/Part1/Data_Preprocessing/data_preprocessing.R
@@ -18,8 +18,22 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
 View(dataset)
 
 # Encoding categorical data
+# Country
 dataset$Country = factor(dataset$Country, 
                          levels = c('France','Spain','Germany'), 
                          labels = c(1,2,3)) #c is vector
+# Purchased
+dataset$Purchased = factor(dataset$Purchased, 
+                         levels = c('No','Yes'), 
+                         labels = c(0,1)) 
 
 
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
