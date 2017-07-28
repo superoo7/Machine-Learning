c9f6903730ec8755e28bfeb8ac6aaea648d86cb0
superoo7 <jlwhoo7@gmail.com>
2017-07-28 11:19:25 +0800

    [Chapter 2] Fitting Simple Linear Regression to the Training set

--- ML/Part2/Simple_Linear_Regression/simple_linear_regression.py
+++ ML/Part2/Simple_Linear_Regression/simple_linear_regression.py
@@ -9,13 +9,13 @@ import pandas as pd
 
 # Import dataset
 dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:,0].values		# Independant variable
-Y = dataset.iloc[:,1].values		# Dependant variable
+X = dataset.iloc[:, 0].values		# Independant variable
+Y = dataset.iloc[:, -1].values		# Dependant variable
 
 # Machine learning prep with spliting dataset for Training set and Test set
 from sklearn.cross_validation import train_test_split
 # normally test_size is .2-.3 (train_size+test_size = 1)
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, random_state = 0)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 1/3, random_state = 0)
 
 # Feature Scaling
 '''
@@ -24,3 +24,30 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 '''
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,Y_train)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
